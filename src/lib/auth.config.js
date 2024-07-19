9d78534617b9434ddc9fd139acce07e79222e8f7
Rehneet11 <rstudy156@gmail.com>
2024-07-19 22:40:03 +0530

    commit 3

--- src/lib/auth.config.js
+++ src/lib/auth.config.js
@@ -22,21 +22,16 @@ export const authConfig = {
     },
     authorized({ auth, request }) {
       const user = auth?.user;
-      const isOnAdminPanel = request.nextUrl?.pathname.startsWith("/admin");
-      const isOnBlogPage = request.nextUrl?.pathname.startsWith("/blog");
+
+
       const isOnLoginPage = request.nextUrl?.pathname.startsWith("/login");
 
       // ONLY ADMIN CAN REACH THE ADMIN DASHBOARD
 
-      if (isOnAdminPanel && !user?.isAdmin) {
-        return false;
-      }
-
+      
       // ONLY AUTHENTICATED USERS CAN REACH THE BLOG PAGE
 
-      if (isOnBlogPage && !user) {
-        return false;
-      }
+      
 
       // ONLY UNAUTHENTICATED USERS CAN REACH THE LOGIN PAGE
 
